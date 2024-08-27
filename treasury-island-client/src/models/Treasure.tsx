3c3b3a538cf2829c673f11138b3d78f1ff13cc47
nicon44 <nicolas@dulio.com.ar>
2024-08-27 13:07:12 -0400

    constraints

--- treasury-island-client/src/models/Treasure.tsx
+++ treasury-island-client/src/models/Treasure.tsx
@@ -1,10 +1,14 @@
-export const Treasure = ({ size = [1, 1] }) => {
+export const Treasure = ({ size = [1, 1], canBeBuried = true }) => {
   const [xSize, ySize] = size;
 
   return (
     <mesh position={[(xSize * 10) / 2 - 5, 2, (ySize * 10) / 2 - 5]}>
       <boxGeometry args={[xSize * 10, 5, ySize * 10]} />
-      <meshStandardMaterial color="violet" />
+      <meshStandardMaterial
+        color={canBeBuried ? "violet" : "red"}
+        transparent={true}
+        opacity={canBeBuried ? 1 : 0.5}
+      />
     </mesh>
   );
 };
--- treasury-island-client/src/providers/GameProvider.tsx
+++ treasury-island-client/src/providers/GameProvider.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren, createContext, useContext, useState } from "react";
-import { IBuriedTreasure, ITreasure } from "../types/Treasure";
 import { Terrain } from "../enums/terrain";
+import { IBuriedTreasure, ITreasure } from "../types/Treasure";
 
 const BASE_GRID = [
   [0, Terrain.PALM, 0, 0, 0, 0],
@@ -17,7 +17,7 @@ const BASE_GRID = [
   [0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, Terrain.PALM, 0],
   [0, 0, 0, 0, 0, 0],
-]
+];
 
 interface IGameContext {
   treasureToBury: ITreasure | undefined;
@@ -27,6 +27,7 @@ interface IGameContext {
   rotateTreasure: () => void;
   grid: Terrain[][];
   updateGridValue: (x: number, y: number, newValue: Terrain) => void;
+  checkIfCanBeBuried: (x: number, y: number) => boolean;
 }
 
 const GameContext = createContext<IGameContext>({
@@ -36,7 +37,8 @@ const GameContext = createContext<IGameContext>({
   buryTreasure: () => {},
   rotateTreasure: () => {},
   grid: BASE_GRID,
-  updateGridValue: () => {}
+  updateGridValue: () => {},
+  checkIfCanBeBuried: () => false,
 });
 export const useGameContext = () => useContext(GameContext);
 
@@ -44,18 +46,30 @@ export const GameProvider = ({ children }: PropsWithChildren) => {
   const [treasureToBury, setTreasureToBury] = useState<ITreasure | undefined>();
   const [buriedTreasures, setBuriedTreasures] = useState<IBuriedTreasure[]>([]);
 
-  const [grid, setGrid] = useState<Terrain[][]>(BASE_GRID)
+  const [grid, setGrid] = useState<Terrain[][]>(BASE_GRID);
+  console.log("grid", grid);
 
   const updateGridValue = (x: number, y: number, newValue: Terrain) => {
-    const newGrid = [...grid];
-    newGrid[x] = [...newGrid[x]];
-    newGrid[x][y] = newValue;
-    setGrid(newGrid);
+    setGrid((prev) => {
+      const newGrid = [...prev];
+      newGrid[x] = [...newGrid[x]];
+      newGrid[x][y] = newValue;
+      return newGrid;
+    });
   };
 
   const buryTreasure = (x: number, y: number) => {
     if (treasureToBury) {
       setBuriedTreasures((prev) => [...prev, { x, y, ...treasureToBury }]);
+      const { xSize, ySize } = treasureToBury;
+      for (let i = 0; i < xSize; i++) {
+        for (let j = 0; j < ySize; j++) {
+          const currentX = x + i;
+          const currentY = y + j;
+          console.log("updating grid value", currentX, currentY);
+          updateGridValue(currentX, currentY, Terrain.TREASURE);
+        }
+      }
     } else {
       console.error("No treasure to bury");
     }
@@ -75,6 +89,28 @@ export const GameProvider = ({ children }: PropsWithChildren) => {
     }
   };
 
+  const checkIfCanBeBuried = (x: number, y: number) => {
+    if (!treasureToBury || !grid) {
+      return false;
+    }
+    const { xSize, ySize } = treasureToBury;
+
+    // Loop through the grid cells that the treasure would occupy
+    for (let i = 0; i < xSize; i++) {
+      for (let j = 0; j < ySize; j++) {
+        const currentX = x + i;
+        const currentY = y + j;
+
+        // Check if the current cell is of Terrain.SAND
+        if (grid[currentX]?.[currentY] !== Terrain.SAND) {
+          return false; // If any cell is not Terrain.SAND, the treasure can't be buried here
+        }
+      }
+    }
+
+    return true; // All cells are Terrain.SAND, so the treasure can be buried here
+  };
+
   return (
     <GameContext.Provider
       value={{
@@ -84,7 +120,8 @@ export const GameProvider = ({ children }: PropsWithChildren) => {
         buryTreasure,
         rotateTreasure,
         grid,
-        updateGridValue
+        updateGridValue,
+        checkIfCanBeBuried,
       }}
     >
       {children}
