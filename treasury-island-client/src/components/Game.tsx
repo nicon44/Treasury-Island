5d0a546025c76697304326042892da87771acfac
nicon44 <nicolas@dulio.com.ar>
2024-08-29 11:55:46 -0400

    add sky

--- treasury-island-client/src/components/Game.tsx
+++ treasury-island-client/src/components/Game.tsx
@@ -1,10 +1,11 @@
+import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useEffect } from "react";
 import { CameraControls } from "../components/CameraControls";
 import { useGameContext } from "../providers/GameProvider";
 import { PhaseProps } from "../types/PhaseProps";
-import { Grid } from "./Grid";
 import CameraTransition from "./CameraTransition"; // Import the new component
+import { Grid } from "./Grid";
 
 export const Game = ({ hide, seek }: PhaseProps) => {
   const { rotateTreasure } = useGameContext();
@@ -32,6 +33,16 @@ export const Game = ({ hide, seek }: PhaseProps) => {
           rotation: [-Math.PI / 3, 0, 0],
         }}
       >
+        <Sky
+          distance={450000} // Camera distance (default=450000)
+          sunPosition={[100, 20, 100]} // Sun position [x, y, z]
+          inclination={0} // Sun elevation angle (default=0)
+          azimuth={0.25} // Sun azimuth angle (default=0.25)
+          turbidity={10} // Sky turbidity (default=10)
+          rayleigh={2} // Sky scattering coefficient (default=2)
+          mieCoefficient={0.005} // Scattering intensity (default=0.005)
+          mieDirectionalG={0.8} // Scattering directional coefficient (default=0.8)
+        />
         <ambientLight intensity={0.5} />
         <directionalLight color="white" position={[5, 5, 5]} />
         <Grid hide={hide} seek={seek} />
